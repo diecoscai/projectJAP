cdec223df636146eb928b5be46bca4a6c551f513
DiegoC <dacc360@gmail.com>
2021-08-11 00:53:10 -0300

    Entrega 1 1.0
    Login
    Products (En proceso)

--- js/login.js
+++ js/login.js
@@ -4,3 +4,21 @@
 document.addEventListener("DOMContentLoaded", function(e){
 
 });
+
+function saveData(emailAddress, password){
+    if(emailAddress.trim()==="" || password.trim()===""){
+        if(emailAddress.trim()===""){
+            alert("Ingrese Nombre de Usuario");
+        }else if(password.trim()===""){
+            alert("Ingrese Contraseña");
+        }
+    }else{
+        localStorage.setItem("user",emailAddress.trim());
+        localStorage.setItem("pass",password.trim());
+        sessionStorage.setItem("user", emailAddress.trim());
+//        alert("Usuario: " + emailAddress + " Contraseña: " + password);
+        location.href="./home.html"
+
+    }
+
+}
