b2efaa4f8197a8c4ea727aa1acda27f3344ad315
DiegoC <dacc360@gmail.com>
2021-08-19 20:48:39 -0300

    hotfix login

--- js/login.js
+++ js/login.js
@@ -1,31 +1,17 @@
-//Función que se ejecuta una vez que se haya lanzado el evento de
-//que el documento se encuentra cargado, es decir, se encuentran todos los
-//elementos HTML presentes.
-document.addEventListener("DOMContentLoaded", function(e){
-        document.getElementById("btnLogin").addEventListener("click"), function (){
-            function saveData(emailAddress, password){
-                if(emailAddress.trim()==="" || password.trim()===""){
-                    if(emailAddress.trim()===""){
-                        emailAddress.classList.add("invalid");
-                        alert("Ingrese Nombre de Usuario");
-                    }else if(password.trim()===""){
-                        password.classList.add("invalid");
-                        alert("Ingrese Contraseña");
-                    }
-                }else{
-                    emailAddress.classList.remove("invalid");
-                    password.classList.remove("invalid");
-                    localStorage.setItem("user",emailAddress.trim());
-                    localStorage.setItem("pass",password.trim());
-                    sessionStorage.setItem("user", emailAddress.trim());
-            //        alert("Usuario: " + emailAddress + " Contraseña: " + password);
-                    location.href="./home.html"
-            
-                }
-            
-            }
+-function saveData(emailAddress, password){
+    if(emailAddress.trim()==="" || password.trim()===""){
+        if(emailAddress.trim()===""){
+            alert("Ingrese Nombre de Usuario");
+        }else if(password.trim()===""){
+            alert("Ingrese Contraseña");
+        }
+    }else{
+        localStorage.setItem("user",emailAddress.trim());
+        localStorage.setItem("pass",password.trim());
+        sessionStorage.setItem("user", emailAddress.trim());
+//        alert("Usuario: " + emailAddress + " Contraseña: " + password);
+        location.href="./home.html"
+
     }
-});
 
-
-
+}
